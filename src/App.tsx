6a01bd632bbe07bf7e434c03cf45406884f852ab
Guilherme Kunsch <suporte@iconexlog.com.br>
2024-02-16 16:13:06 -0300

    subindo projeto

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import { Sidebar } from "./components/Sidebar.tsx";
+import { Header } from "./components/Header.tsx";
+import { Post } from "./components/Post.tsx";
+import "./global.css";
+import styles from "./App.module.css";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/75049024?v=4',
+      name: 'Guilherme Künsch',
+      role: 'Desenvolvedor Web'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ], 
+    publishedAt: new Date('2024-01-13 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/66263035?v=4',
+      name: 'Iconex Logística',
+      role: 'Empresa'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ], 
+    publishedAt: new Date('2024-01-09 20:00:00'),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Comment.tsx
@@ -0,0 +1,58 @@
+import styles from "./Comment.module.css";
+import { Trash } from "phosphor-react";
+import { ThumbsUp } from "phosphor-react";
+import { Avatar } from "./Avatar";
+import { useState } from "react";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    })
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://avatars.githubusercontent.com/u/75049024?v=4"
+        alt = 'Desenvolvedor'
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Guilherme Kunsch</strong>
+              <time title="10 de Janeiro de 2024" dateTime="2024-01-10 23:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button onClick={handleDeleteComment} title="deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp size={20} />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
